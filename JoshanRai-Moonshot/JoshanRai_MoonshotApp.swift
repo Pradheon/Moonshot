e23a798a5b885c9924df502bb3de8e3828c2fe51
Joshan Rai <pradheon@gmail.com>
2022-03-14 14:35:46 -0700

    Initial Commit

---
+++ JoshanRai-Moonshot/JoshanRai_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_MoonshotApp.swift
+//  JoshanRai-Moonshot
+//
+//  Created by Joshan Rai on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
