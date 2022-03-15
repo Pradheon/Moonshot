7c1326188424f49a57642de7ef9831ec87e17a8f
Joshan Rai <pradheon@gmail.com>
2022-03-14 17:44:01 -0700

    v0.6 - Loading a specific kind of Codable data - Added 'astronauts.json' and 'missions.json'. - Added images from NASA for astronauts and missions.

---
+++ JoshanRai-Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  JoshanRai-Moonshot
+//
+//  Created by Joshan Rai on 3/14/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
